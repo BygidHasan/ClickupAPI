3b9c594737bd66fd1cb5954275483325a22d982b
MD Bygid Hasan <bygidhasan6@gmail.com>
2025-03-26 00:30:55 +0600

    added two urls of codechef and leetcode

--- utils/urls.js
+++ utils/urls.js
@@ -1,5 +1,9 @@
 const codeforcesUrl = 'https://codeforces.com/api/contest.list';
+const codeChefUrl = 'https://kontests.net/api/v1/code_chef';
+const leetCodeUrl = 'https://kontests.net/api/v1/leet_code';
 
 module.exports = {
-  codeforcesUrl
+  codeforcesUrl,
+  codeChefUrl,
+  leetCodeUrl
 };
